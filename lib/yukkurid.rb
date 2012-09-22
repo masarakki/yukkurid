6bca58eb71ac37670c8cd6fd6f63ef3db858af35
masarakki <masaki@hisme.net>
2012-09-23 07:17:41 +0900

    listen nicolive and yukkuri

--- lib/yukkurid.rb
+++ lib/yukkurid.rb
@@ -1,9 +1,29 @@
 require 'sinatra'
+require 'socket'
+require 'nokogiri'
+
+def say(message)
+  yukkuri_bin = File.expand_path('../../bin/yukkuri', __FILE__)
+  `echo #{message} | nkf -e | mecab -Oyomi | nkf -w | #{yukkuri_bin} |aplay -q`
+end
+
+post '/listen_nicolive' do
+  addr = params[:addr]
+  port = params[:port].to_i
+  thread = params[:thread].to_i
+
+  socket = TCPSocket.new addr, port
+  socket.sendmsg("<thread thread=\"#{thread}\" version=\"20061206\" res_from=\"-1\" />\0")
+  loop {
+    recv = socket.recvmsg
+    text = Nokogiri::XML(recv.first.strip).text.strip
+    say(text) unless text == ""
+  }
+end
 
 post '/' do
   if params[:message]
-    yukkuri_bin = File.expand_path('../../bin/yukkuri', __FILE__)
-    `echo #{params[:message]} | nkf -e | mecab -Oyomi | nkf -w | #{yukkuri_bin} |aplay -q`
+    say params[:message]
   end
   ""
 end
