ea807f81158b1626cd5aaf0d1b424712aa91125d
masarakki <masaki@hisme.net>
2012-10-03 05:49:37 +0900

    split word for mora

--- lib/yukkuri/message.rb
+++ lib/yukkuri/message.rb
@@ -9,12 +9,26 @@ module Yukkuri
     ACCENT = 23
     CONBINATION_TYPE = 24
 
+
     def initialize(str)
       @message = str
     end
 
     def mecab
       @mecab ||= CSV.parse(`echo "#{@message}" | mecab -d ~/unidic/dic/unidic-mecab --eos-format=\n`.gsub(/\t/, ','))
+    end
+
+    def mora
+      @mora ||= mecab.map do |word|
+        (word[YOMI] || word[0]).each_char.inject [] do |res, char|
+          if %{ャ ュ ョ ァ ィ ゥ ェ ォ}.include? char
+            before = res.pop
+            res.push before + char
+          else
+            res << char
+          end
+        end
+      end
     end
 
     def yomi
@@ -32,12 +46,11 @@ module Yukkuri
       before_accent = 0
 
       mecab.each_with_index do |word, index|
-        yomi = word[YOMI]
         accent = word[ACCENT] == "*" || word[ACCENT].nil? ? 0 : word[ACCENT].split(",").first.to_i
         before_hinshi = index > 0 ? mecab[index-1][HINSHI] : ""
         case word[CONBINATION_TYPE]
         when /C/
-          if mecab[index-1][HINSHI] == "名詞"
+          if index > 0 && mecab[index-1][HINSHI] == "名詞"
             case word[CONBINATION_TYPE]
             when "C1"
               result[index - 1] = 0 if index > 0
@@ -46,7 +59,7 @@ module Yukkuri
               result[index - 1] = 0 if index > 0
               result[index] = 1
             when "C3"
-              result[index - 1] = mecab[index - 1][YOMI].length if index > 0
+              result[index - 1] = mora[index - 1].length if index > 0
               result[index] = 0
             when "C4"
               result[index - 1] = 0 if index > 0
@@ -58,7 +71,7 @@ module Yukkuri
             result[index] = accent
           end
         when /F/
-          regex = Regexp.new("#{before_hinshi}%(F\\d)(@(\\d)(,(\\d))?)?")
+          regex = Regexp.new("#{before_hinshi}%(F\\d)(@(-?\\d)(,(-?\\d))?)?")
           matches = word[CONBINATION_TYPE].match(regex)
           if matches
             type = matches[1]
@@ -68,7 +81,12 @@ module Yukkuri
             when "F2"
               m = matches[3].to_i
               if result[index - 1] == 0
-                result[index] = m
+                if m < 0
+                  result[index-1] = result[index-1]
+                  result[index] = 0
+                else
+                  result[index] = m
+                end
               else
                 result[index] = 0
               end
@@ -100,6 +118,22 @@ module Yukkuri
           else
             result[index] = 0
           end
+        when /P/
+          next_mora = mora[index+1].length
+          next_accent_type = mecab[index+1][ACCENT].to_i
+          case word[CONBINATION_TYPE]
+          when "P2"
+            if next_mora == next_accent_type || next_accent_type == 0
+              result[index] = 0
+              result[index+1] = 1
+              mecab[index+1][ACCENT] = "1"
+            else
+              result[index] = 0
+              result[index+1] = next_accent_type
+            end
+          else
+            result[index] = 0
+          end
         else
           result[index] = accent
         end
@@ -107,15 +141,15 @@ module Yukkuri
       before_accent = false
 
       mecab.each_with_index.map do |word, index|
-        yomi = word[YOMI] || word[0]
+        yomi = mora[index].dup
         yomi.insert(result[index], "'") if result[index] != 0
         yomi << "/" if ["助詞", "副詞", "助動詞"].include?(word[HINSHI]) && index < mecab.length - 1 && !["助詞", "副詞", "助動詞", "補助記号"].include?(mecab[index + 1][HINSHI])
-        yomi
+        yomi.join
       end.join
     end
 
     def play
-      `echo #{aqtalk} | /home/masaki/workspace/yukkurid/bin/yukkuri | aplay -q`
+      `echo "#{aqtalk}" | /home/masaki/workspace/yukkurid/bin/yukkuri | aplay -q`
     end
   end
 end
--- spec/yukkuri/message_spec.rb
+++ spec/yukkuri/message_spec.rb
@@ -4,34 +4,88 @@ require 'spec_helper'
 describe Yukkuri::Message do
   subject { described_class.new self.class.ancestors[1].description }
 
-  context "解析結果を表示します" do
-    its(:aqtalk) { should == "カイセキケ'ッカオ/ヒョージシマ'ス" }
+  describe :mora do
+    subject { described_class.new "カッパの好物はチョコレート" }
+    its("mora.flatten") { should == %w{カ ッ パ ノ コ ー ブ ツ ワ チョ コ レ ー ト} }
   end
 
-  context "生活" do
-    its(:aqtalk) { should == "セ'ーカツ" }
-  end
 
-  context "社会生活" do
-    its(:aqtalk) { should == "シャカイセ'ーカツ" }
-  end
+  describe "アクセント" do
+    context "解析結果を表示します" do
+      its(:aqtalk) { should == "カイセキケ'ッカオ/ヒョージシマ'ス" }
+    end
 
-  context "これは、合成音声です。" do
-    its(:aqtalk) { should == "コレワ、ゴーセーオ'ンセーデス。" }
-  end
+    context "アクセント結合型 普通名詞・接尾辞" do
+      context "C1" do
+        context "大将" do
+          its(:aqtalk) { should == "タ'イショー" }
+        end
+        context "大将手続き" do
+          its(:aqtalk) { should == "タイショーテツ'ズキ" }
+        end
+      end
 
-  context "これは、音声記号です。" do
-    its(:aqtalk) { should == "コレワ、オンセーキ'ゴーデス。" }
-  end
+      context "C2" do
+        context "生活" do
+          its(:aqtalk) { should == "セーカツ" }
+        end
 
-  context "今度は、もう少し複雑な音声記号です。" do
-    before { p subject.accent }
-    its(:aqtalk) { should == "コ'ンドワ、モースコ'シ/フクザツナ/オンセーキ'ゴーデス。" }
-  end
+        context "社会生活" do
+          its(:aqtalk) { should == "シャカイセ'ーカツ" }
+        end
+      end
 
-  context "それは難しいな" do
-    before { p subject.accent }
-    its(:aqtalk) { should == "ソレワ/ムズカシ'ーナ" }
+      context "C3" do
+        context "経済" do
+          its(:aqtalk) { should == "ケ'ーザイ" }
+        end
+        context "経済学" do
+          its(:aqtalk) { should == "ケーザイ'ガク" }
+        end
+      end
+      context "C4" do
+        context "物理" do
+          its(:aqtalk) { should == "ブ'ツリ" }
+        end
+        context "物理系" do
+          its(:aqtalk) { should == "ブツリケー" }
+        end
+      end
+    end
+
+    context "これは、合成音声です。" do
+      its(:aqtalk) { should == "コレワ、ゴーセーオ'ンセーデス。" }
+    end
+
+    context "これは、音声記号です。" do
+      its(:aqtalk) { should == "コレワ、オンセーキ'ゴーデス。" }
+    end
+
+    context "今度は、もう少し複雑な音声記号です。" do
+      its(:aqtalk) { should == "コ'ンドワ、モースコ'シ/フクザツナ/オンセーキ'ゴーデス。" }
+    end
+
+    context "それは難しいな" do
+      before { p subject.accent }
+      its(:aqtalk) { should == "ソレワ/ムズカシ'ーナ" }
+    end
+
+    context "ご想像にお任せします" do
+      its(:aqtalk) { should == "ゴソーゾーニ/オマ'カセシマ'ス" }
+    end
+
+    context "任せてもいいですか?" do
+      its(:aqtalk) { should == "マカセ'テモ/イ'ーデスカ\?" }
+    end
+
+    context "今日は非常に暑いので、夜はビールでも飲みに行きたいと思っています。" do
+      its(:aqtalk) { should == "キョ'ーワ/ヒジョーニ/アツ'イノデ、ヨ'ルワ/ビ'ールデモ/ノ'ミニ/イキタ'イト/オモ'ッテ/イマ'ス。" }
+    end
+
+    context "両軍の総大将" do
+      its(:aqtalk) { should == "リョーグンノ/ソータ'イショー" }
+    end
   end
 end
+
 
